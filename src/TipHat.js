f80e6a925c0a65db93a7c4768b89a4296dd2892e
Alex Martin <alex@subtleco.co>
2021-03-15 18:03:10 -0500

    installed router, renamed main app

---
+++ src/TipHat.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const TipHat = props => {
+    return(
+        <h1>Hello from TipHat.js!</h1>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { TipHat } from './TipHat';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TipHat />
   </React.StrictMode>,
   document.getElementById('root')
 );
