b36d86251c7ee3f9de1c30a1cc29374ce526bb20
Alex Martin <alex@subtleco.co>
2021-03-17 08:59:02 -0500

    Working on selects for API call

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -3,7 +3,6 @@ import { Route } from 'react-router-dom'
 import { Register } from './auth/Register'
 import { UserProvider } from './auth/UserProvider'
 import { Home } from './home/Home'
-import { LiveReport } from './liveReports/LiveReport'
 import { LiveReportForm } from './liveReports/LiveReportForm'
 
 export const ApplicationViews = () => {
--- src/components/liveReports/LiveReportForm.js
+++ src/components/liveReports/LiveReportForm.js
@@ -12,7 +12,29 @@ export const LiveReportForm = () => {
     useEffect(() => {
         getCurrentUser()
     }, [])
+// API Key Format
+// http://ws.audioscrobbler.com/2.0/?mthod=user.gettopartists&user={username}&api_key={key}&format=json
 
+    const typeArray = [
+        'Select Report Type',
+        'artists',
+        'albums',
+        'tracks'
+    ]
+
+    const typeOptions = typeArray.map((type, i) => <option value={i}>{type}</option>)
+
+    const periodArray = [
+        {unselected: 'Select A Listening Period'},
+        {'overall': 'all time'},
+        {'7day': 'the last week'},
+        {'1month': 'the last month'},
+        {'3month': 'the last 3 months'},
+        {'6month': 'the last 6 months'},
+        {'12month': 'the last year'}
+    ]
+
+    const periodOptions = periodArray.map((period, i) => <option value={i}>{Object.values(period)[0]}</option>)
 
     return (
         <form className="report__api__form">
@@ -20,16 +42,21 @@ export const LiveReportForm = () => {
             <h4>Using your last.fm username <strong>{currentUser.lastFmAccount}</strong></h4>
             <div className="api__form__selects">
                 <fieldset>
-                    <label htmlFor="period">Report Type:</label>
+                    <p className="api__form__p">I'd like to see my top</p>
+                    <input type='number' min="5" max="50" default="25" placeholder="25"></input>
+                </fieldset>
+                <fieldset>
+                    <select id="type">
+                        {typeOptions}
+                    </select>
+                </fieldset>
+                <fieldset>
+                    <p className="api__form__p">for</p>
                     <select id="period">
-                        {/* //======================================================================// */}
-                        {/* Make these a .map from an array */}
-                        <option>Artist</option>
-                        <option>Album</option>
+                        {periodOptions}
                     </select>
                 </fieldset>
             </div>
-
         </form>
     )
 }
