357cbddf3c12b8a5b14935eef8bc02d296927bcc
Alex Martin <alex@subtleco.co>
2021-03-24 15:03:45 -0500

    Installed Material-ui, added AppBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,28 +1,41 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './NavBar.css'
-import hat from '../../images/hat.png'
+import { makeStyles } from '@material-ui/core'
+import AppBar from '@material-ui/core/AppBar'
+import IconButton from '@material-ui/core/IconButton'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import MenuIcon from '@material-ui/icons/Menu'
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+}))
 
 export const NavBar = () => {
+    const classes = useStyles()
     const userId = parseInt(sessionStorage.getItem('app_user_id'))
-
     const logout = () => {
         sessionStorage.clear('app_user_id')
     }
     return (
-        <div className="nav">
-            <img id="logo" src={hat} height="100"></img>
-            <div className="nav__right">
-                <h1>Tip Hat</h1>
-                <nav id="navbar">
-                    <Link to="/">Why Tip?</Link>
-                    <Link to="/reports/create">Generate A Report</Link>
-                    <Link to="/reports">Saved Reports</Link>
-                    <Link to="/user/edit">(Account)</Link>
-                    <Link to="/" onClick={logout}>(log out)</Link>
-
-                </nav>
-            </div>
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar variant="dense">
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" color="inherit">
+                        Item
+                    </Typography>
+                </Toolbar>
+            </AppBar>
         </div>
     )
 }
+
