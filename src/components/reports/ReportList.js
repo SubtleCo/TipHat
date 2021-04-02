ecef0a30c727e1b154263731bdd97d71ea0bf07b
Alex Martin <alex@subtleco.co>
2021-04-02 12:16:49 -0500

    select now inline in the TH for saved plans too

--- src/components/reports/ReportList.js
+++ src/components/reports/ReportList.js
@@ -1,16 +1,32 @@
 // This module gets all plans from the DB, filters them to the current user, sorts them in order of timestamp, and divides them into paid / unpaid plans.
 // Each plan is then afforded a "ReportCard"
 
+import { makeStyles, Typography } from '@material-ui/core'
 import React, { useContext, useEffect, useState } from 'react'
 import { currentUser, getCurrentUser } from '../auth/UserProvider'
 import { PlanContext } from '../plans/PlanProvider'
 import { ReportCard } from './ReportCard'
 import './ReportList.css'
 
+const useStyles = makeStyles(theme => ({
+    section: {
+        margin: theme.spacing(4),
+        padding: theme.spacing(2),
+        border: "1px solid black",
+        borderRadius: 5,
+        backgroundColor: theme.palette.primary.contrastText
+    },
+    title: {
+        textAlign: "center",
+        marginBottom: theme.spacing(4)
+    }
+}))
+
 export const ReportList = props => {
     const { plans, getPlans } = useContext(PlanContext)
     const [displayPaidPlans, setDisplayPaidPlans] = useState([])
     const [displayUnpaidPlans, setDisplayUnpaidPlans] = useState([])
+    const classes = useStyles(props.theme)
 
     useEffect(() => {
         getPlans()
@@ -29,17 +45,17 @@ export const ReportList = props => {
     return (
         
         <section className="savedReports--container main__container">
-            <h2>Your Saved Reports</h2>
+            <Typography component="h2" variant="h2" align='center'>Your Saved Reports</Typography>
 
-            <section id="savedReports__unpaid">
-                <h3 className="savedReports__section--title">Unpaid Reports</h3>
+            <section className={classes.section}>
+                <Typography variant="h4" className={classes.title}>Unpaid Reports</Typography>
                 {
                     displayUnpaidPlans.map(dP => <ReportCard key={dP.id} report={dP} />)
                 }
             </section>
 
-            <section id="savedReports__paid">
-                <h3 className="savedReports__section--title">Paid Reports</h3>
+            <section className={classes.section}>
+                <Typography variant="h4" className={classes.title}>Paid Reports</Typography>
                 {
                     displayPaidPlans.map(dP => <ReportCard key={dP.id} report={dP} />)
                 }
